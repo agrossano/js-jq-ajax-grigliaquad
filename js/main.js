ae23c7c3210c511aebdfa9072e26376e2ef0efd1
Alessandro Grossano <grossano.alessandro@gmail.com>
2020-04-17 16:34:26 +0200

    Ex base terminato

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/* DESCRIZIONE:
+Griglia 6x6,
+ad ogni click (su ogni rettangolino) parte una richiesta AJAX che prende un numero random da 1 a 9 (primo end-point della API in slide).
+Se il num ritornato è <= 5 il quadrato diventa giallo,
+se è > di 5 il quadrato diventa verde.
+Il numero ottenuto appare al centro del quadrato
+BONUS: (ma solo se il resto è fatto)
+se utente clicca 2 volte sullo stesso rettangolo lo mando a ca… gli dico che non si può;
+generare dinamicamente la griglia dei quadrati;
+Curo per bene l’output, creando un layout carino;
+varie che vi vengono in mente per sperimentare;
+ */
+
+
+$(document).ready(function () {
+
+
+
+  $(".box").click(function () {
+    var element = this
+    $.ajax({
+      url: "https://flynn.boolean.careers/exercises/api/random/int",
+      method: "GET",
+      success: function (data, stato) {
+        if (data.response <= 5) {
+          $(element).removeClass("green")
+          $(element).addClass("yellow")
+        } else {
+          $(element).removeClass("yellow")
+          $(element).addClass("green")
+        }
+      }
+    });
+
+  });
+
+
+
+
+
+});
